afbda977ce9193c86b36a35a445cf00611ace635
abiramikumaresan <34230155+abiramikumaresan@users.noreply.github.com>
2018-04-12 18:59:48 +0530

    Add files via upload

---
+++ demosimpleApplication.java
@@ -0,0 +1,18 @@
+ import org.springframework.boot.SpringApplication;
+
+
+
+ @SpringBootApplication
+
+ public class DemosimpleApplication
+ 	{
+
+	
+	  public static void main(String[] args)
+ 	  {
+		
+          SpringApplication.run(DemoabiiApplication.class, args);
+	
+	  }
+
+        }
