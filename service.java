f0c061d0d3398d993b73e79128f5e5d3a6eb750c
abiramikumaresan <34230155+abiramikumaresan@users.noreply.github.com>
2018-04-25 20:25:47 +0530

    Add files via upload

---
+++ service.java
@@ -0,0 +1,11 @@
+package com.i18n.demoabii;
+import org.springframework.stereotype.Component;
+@Component
+public class service 
+{
+	public String showservice()
+	{
+		return"hi abi this is service class";
+	}
+
+}
