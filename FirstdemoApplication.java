91f196b1a585c91b3aba0f3eb36b3f983c33e8ee
abiramikumaresan <34230155+abiramikumaresan@users.noreply.github.com>
2018-05-05 20:59:25 +0530

    Add files via upload

---
+++ FirstdemoApplication.java
@@ -0,0 +1,23 @@
+package com.i18n.firstdemo;
+
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+public class FirstdemoApplication 
+{
+
+	
+	public static void main(String[] args) 
+	{
+
+		SpringApplication.run(FirstdemoApplication.class, args);
+	
+	}
+
+}
+
