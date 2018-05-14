8f0f598a737e5fcc32c8a5b61619b20549d0b9ef
abiramikumaresan <34230155+abiramikumaresan@users.noreply.github.com>
2018-05-14 19:38:54 +0530

    Add files via upload

---
+++ serv3.java
@@ -0,0 +1,24 @@
+package com.i18n.firstdemo;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.springframework.stereotype.Component;
+@Component
+public class serv3 {
+	List<student> l1=new ArrayList<student>(Arrays.asList(new student("Abi",21,01,"i18n"),(new student("Balu",25,02,"i18n"))));
+
+	public List<student> getservice()
+	{
+		return l1;
+	}
+	public void add(student s)
+	{
+		l1.add(s);		
+	}
+
+	public student getservice(String nam)
+	{
+			return l1.stream().filter(t->t.getName().equals(nam)).findFirst().get();
+	}
+}
